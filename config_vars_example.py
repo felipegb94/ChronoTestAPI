b7caf7f5fd79b280cfef30290ed53db1e2ee347a
Felipe Gutierrez <fgutierrez3@wisc.edu>
2015-02-03 14:00:02 -0600

    config_vars example file added

---
+++ config_vars_example.py
@@ -0,0 +1,4 @@
+
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/NAME OF DATABASE GOES HERE"
+SECRET_KEY = "Your secret key goes here, this can be anything random phrase."
+HMAC_KEY = '' #NOT USED RIGHT NOW
